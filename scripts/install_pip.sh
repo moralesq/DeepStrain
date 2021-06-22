9520c8566c8fdf1f375d4cde08e0781c1d26ce0d
moralesq <moralesq@mit.edu>
2021-06-22 12:46:05 -0400

    removed sudo command

--- scripts/install_pip.sh
+++ scripts/install_pip.sh
@@ -1,7 +1,7 @@
 set -ex
 
-sudo -H pip install gdown
-sudo -H pip install nibabel
-sudo -H pip install dicom2nifti
-sudo -H pip install dipy
-sudo -H pip install pandas
+pip install gdown
+pip install nibabel
+pip install dicom2nifti
+pip install dipy
+pip install pandas
