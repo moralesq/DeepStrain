956de034055bd44bab87157b0a9cba97dce97bdf
moralesq <moralesq@mit.edu>
2021-10-22 11:43:54 -0400

    added documentation to metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -36,10 +36,10 @@ def get_geometric_metrics(M_gt, M_pred, voxelspacing,
     return pd.DataFrame(output)
         
 def get_volume_ml(M, voxel_spacing_mm, tissue_label=1):
-    voxel_vol_cm3 = np.prod(voxel_spacing_mm) / 1000
-    if len(M.shape) == 4:
-        M = np.argmax(M, -1)
+
+    voxel_vol_cm3 = np.prod(voxel_spacing_mm) / 1000 
     volume_ml = (M==tissue_label).sum()*voxel_vol_cm3
+
     return volume_ml
 
 def get_mass_g(M, voxel_spacing_mm, tissue_label=2, tissue_density_g_per_ml=1.05):
@@ -63,12 +63,56 @@ def get_clinical_parameters_lv(M_ed, M_es, voxel_spacing_mm):
     return LV_EDV_ml, LV_ESV_ml, LV_EF, LV_mass_g
 
 def get_clinical_parameters(M_ed, M_es, voxel_spacing_mm):
+    """Generate left- and right-ventricular parameters using a mask of the myocardium. Mask values should be: 
+
+    0 background 
+    1 right-ventricular blood pool 
+    2 left-ventricular myocardium 
+    3 left-ventricular blood pool 
+
+    Input
+    -----
+    M_ed             : 3D array containing binary labels for the myocardium at end-diastole. 
+    M_es             : 3D array containing binary labels for the myocardium at end-systole. 
+    voxel_spacing_mm : tuple containing spatial resolution of 3D volume in mm, i.e., (dx, dy, dz)
+
+    Output
+    ------
+    clinical_parameters : dictionary of cardiac parameters
+
+    Example
+    -------
+    import nibabel as nib
+    from aux import metrics 
+
+    >>> # load cine image and corresonding segmentation, each of shape (nx, ny, nz, nt)
+    >>> V_nifti = nib.load('sample.nii.gz')
+    >>> M_nifti = nib.load('sample_segmentation.nii.gz')
+
+    >>> # get the 4D array of size (nx,ny,nz,nt)
+    >>> V = V_nifti.get_fdata()
+    >>> M = M_nifti.get_fdata()
+
+    >>> # get the spatial resolution 
+    >>> resolution = M_nifti.header.get_zooms()[:3]
+    >>> print(M.shape, resolution)
+    (256, 256, 7, 30) (1.40625, 1.40625, 8.0)
+    >>> # Assume diastole is at t=0, systole at t=10
+    >>> print(M[...,0].shape)
+    (256, 256, 7)
+    >>> params = metrics.get_clinical_parameters(M_ed=M[...,0], M_es=M[...,10], voxel_spacing_mm=resolution)
+    >>> print(params)
+    {'RV_EDV_ml': 69.1189453125, 'RV_ESV_ml': 17.370703125, 'RV_EF': 74.868390936141, 
+     'LV_EDV_ml': 51.526757812499994, 'LV_ESV_ml': 25.5181640625, 'LV_EF': 50.47589806570464, 'LV_mass_g': 73.903798828125}
+    >>> print(params['LV_EDV_ml'])
+    51.526757812499994
+    """
+    #print(M_ed.shape)
     RV_EDV_ml, RV_ESV_ml, RV_EF = get_clinical_parameters_rv(M_ed, M_es, voxel_spacing_mm)
     LV_EDV_ml, LV_ESV_ml, LV_EF, LV_mass_g = get_clinical_parameters_lv(M_ed, M_es, voxel_spacing_mm)
     clinical_parameters = {'RV_EDV_ml':RV_EDV_ml, 'RV_ESV_ml':RV_ESV_ml, 'RV_EF':RV_EF, 
                            'LV_EDV_ml':LV_EDV_ml, 'LV_ESV_ml':LV_ESV_ml, 'LV_EF':LV_EF, 'LV_mass_g':LV_mass_g}
     return clinical_parameters
-
 
 ## Stats
 
